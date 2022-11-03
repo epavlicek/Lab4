8ec65b70c54fa5d572fa1eb73f8c8053d998ee39
ep <empavlicek@gmail.com>
2022-11-03 11:59:17 -1000

    third commit

---
+++ frontend/src/post/CreatePost.js
@@ -0,0 +1,73 @@
+import { useState, useContext, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { useResource } from "react-request-hook";
+import { StateContext } from "../contexts";
+
+export default function CreatePost() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [error, setError] = useState(false);
+
+  const { state, dispatch } = useContext(StateContext);
+  const { user } = state;
+
+  const [post, createPost] = useResource(({ title, content, author }) => ({
+    url: "/posts",
+    method: "post",
+    data: { title, content, author },
+  }));
+  useEffect(() => {
+    if (post?.error) {
+      setError(true);
+      //alert("Something went wrong creating post.");
+    }
+    if (post?.isLoading === false && post?.data) {
+      dispatch({
+        type: "CREATE_POST",
+        title: post.data.title,
+        content: post.data.content,
+        author: post.data.author,
+        id: post.data.id,
+      });
+    }
+  }, [post]);
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        createPost({ title, content, author: user });
+        dispatch({
+          type: "CREATE_POST",
+          title,
+          content,
+          //ndate,
+          author: user,
+          id: uuidv4(),
+        });
+      }}
+    >
+      <div>
+        Author: <b>{user}</b>
+      </div>
+      <div>
+        date: <b>{Date.now()}</b>
+      </div>
+      <div>
+        <label htmlFor="create-title">Title:</label>
+        <input
+          type="text"
+          name="create-title"
+          id="create-title"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+      </div>
+      <textarea
+        value={content}
+        onChange={(event) => setContent(event.target.value)}
+      />
+      <input type="submit" value="Add to List" />
+    </form>
+  );
+}
